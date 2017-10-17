1a8c3d8859947b26c5bb1d65aeab51593b1bc211
Maria Korshikova <mariyakorsh@yandex.ru>
2017-10-18 00:46:59 +0300

    created project angular-cli, added header-component

---
+++ src/components/content/content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+import './content.component.css'
+@Component({
+  selector: 'content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent{
+ }
---
+++ src/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+import './header.component.css';
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+}
